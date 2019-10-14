b2e4ef5af78d9917307ffd6afc1ae501fe4ebaca
Francesco Biscani <bluescarni@gmail.com>
2019-10-15 00:17:42 +0200

    Try to retain existing CXXFLAGS, and apply the osx fix at link time only.

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,13 +1,17 @@
 mkdir -p build
 cd build
+
 # NOTE: -fPIC is needed in order for abseil's
 # static libraries to play well with dynamically
 # loaded plugins (e.g., python modules).
+export CXXFLAGS="$CXXFLAGS -fPIC"
+
 cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} \
 	  -DCMAKE_PREFIX_PATH=${PREFIX} \
 	  -DCMAKE_INSTALL_LIBDIR=lib \
 	  -DCMAKE_BUILD_TYPE=Release \
-	  -DCMAKE_CXX_FLAGS=-fPIC \
 	  ..
+
 make -j${CPU_COUNT} VERBOSE=1
+
 make install
