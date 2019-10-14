786ba45e521eaa073f9c1ec95a662808671fbaa8
Francesco Biscani <bluescarni@gmail.com>
2019-10-14 23:19:29 +0200

    Enable the -fPIC flag for Unix builds.

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,6 +4,10 @@ cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} \
 	  -DCMAKE_PREFIX_PATH=${PREFIX} \
 	  -DCMAKE_INSTALL_LIBDIR=lib \
 	  -DCMAKE_BUILD_TYPE=Release \
+	  # NOTE: -fPIC is needed in order for abseil's
+	  # static libraries to play well with dynamically
+	  # loaded plugins (e.g., python modules).
+	  -DCMAKE_CXX_FLAGS=-fPIC \
 	  ..
 make -j${CPU_COUNT}
 make install
