37458a306bd18d805054da09e5dfeb480079407f
Francesco Biscani <bluescarni@gmail.com>
2019-10-14 23:50:15 +0200

    Script fix.

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,12 +1,12 @@
 mkdir -p build
 cd build
+# NOTE: -fPIC is needed in order for abseil's
+# static libraries to play well with dynamically
+# loaded plugins (e.g., python modules).
 cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} \
 	  -DCMAKE_PREFIX_PATH=${PREFIX} \
 	  -DCMAKE_INSTALL_LIBDIR=lib \
 	  -DCMAKE_BUILD_TYPE=Release \
-	  # NOTE: -fPIC is needed in order for abseil's
-	  # static libraries to play well with dynamically
-	  # loaded plugins (e.g., python modules).
 	  -DCMAKE_CXX_FLAGS=-fPIC \
 	  ..
 make -j${CPU_COUNT}
