0796fd0979e975230052875a241e50d0475e4d3b
Francesco Biscani <bluescarni@gmail.com>
2019-10-15 00:34:42 +0200

    -fPIC is actually already implied by the conda flags.

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,11 +1,6 @@
 mkdir -p build
 cd build
 
-# NOTE: -fPIC is needed in order for abseil's
-# static libraries to play well with dynamically
-# loaded plugins (e.g., python modules).
-export CXXFLAGS="$CXXFLAGS -fPIC"
-
 cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} \
 	  -DCMAKE_PREFIX_PATH=${PREFIX} \
 	  -DCMAKE_INSTALL_LIBDIR=lib \
