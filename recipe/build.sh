803d447bc44a52a41a6f4f11c479cf24a1fcf5a5
Uwe L. Korn <uwe.korn@quantco.com>
2020-07-21 16:44:26 +0200

    CFEP-18: Remove static libraries from main build

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,6 +5,7 @@ cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} \
 	  -DCMAKE_PREFIX_PATH=${PREFIX} \
 	  -DCMAKE_INSTALL_LIBDIR=lib \
 	  -DCMAKE_BUILD_TYPE=Release \
+      -DBUILD_SHARED_LIBS=ON \
       -GNinja \
 	  ..
 
