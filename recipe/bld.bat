adeffe53cb549b6a13ba59ce8f5c3984591e953b
H. Vetinari <h.vetinari@gmx.com>
2022-03-02 14:18:18 +1100

    use ABSL_PROPAGATE_CXX_STD=ON and set C++17 also on windows
    switches to the non-deprecated option, but also needs to patch
    upstream to actually propagate C++17 instead of C++11, compare:
    https://github.com/abseil/abseil-cpp/blob/20211102.0/CMake/AbseilHelpers.cmake#L257-L273

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,11 +2,18 @@
 
 mkdir build
 cd build
+
+REM regarding ABSL_PROPAGATE_CXX_STD:
+REM enforce consuming libraries to compile using at least the same C++ standard, see
+REM patches/0006-propagate-requirement-of-C-17-to-consumers.patch as well as
+REM https://github.com/abseil/abseil-cpp/blob/20211102.0/CMake/AbseilHelpers.cmake#L257-L273
 cmake -GNinja ^
+    -DABSL_PROPAGATE_CXX_STD=ON ^
+    -DBUILD_SHARED_LIBS=ON ^
+    -DCMAKE_BUILD_TYPE=Release ^
+    -DCMAKE_CXX_STANDARD=17 ^
     -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
     -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
-    -DCMAKE_BUILD_TYPE=Release ^
-    -DBUILD_SHARED_LIBS=ON ^
     ..
 if %ERRORLEVEL% neq 0 exit 1
 
--- recipe/build.sh
+++ recipe/build.sh
@@ -5,7 +5,12 @@ set -exuo pipefail
 mkdir -p build
 cd build
 
+# regarding ABSL_PROPAGATE_CXX_STD:
+# enforce consuming libraries to compile using at least the same C++ standard, see
+# patches/0006-propagate-requirement-of-C-17-to-consumers.patch as well as
+# https://github.com/abseil/abseil-cpp/blob/20211102.0/CMake/AbseilHelpers.cmake#L257-L273
 cmake ${CMAKE_ARGS} \
+    -DABSL_PROPAGATE_CXX_STD=ON \
     -DBUILD_SHARED_LIBS=ON \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_CXX_STANDARD=17 \
