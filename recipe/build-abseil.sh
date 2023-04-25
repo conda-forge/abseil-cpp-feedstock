5d01c46dfd2737623dafad0030248e560c706e33
H. Vetinari <h.vetinari@gmx.com>
2023-04-25 19:31:13 +1100

    add separate output for test targets

--- recipe/build-abseil.sh
+++ recipe/build-abseil.sh
@@ -10,14 +10,19 @@ if [[ "${target_platform}" == osx-* ]]; then
     CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
 fi
 
-cmake ${CMAKE_ARGS} \
+if [[ "$PKG_NAME" == "libabseil-tests" ]]; then
+    CMAKE_ARGS="${CMAKE_ARGS} -DBUILD_TESTING=ON -DABSL_BUILD_TESTING=ON"
+    CMAKE_ARGS="${CMAKE_ARGS} -DABSL_USE_EXTERNAL_GOOGLETEST=ON -DABSL_FIND_GOOGLETEST=ON"
+fi
+
+cmake -G Ninja \
+    ${CMAKE_ARGS} \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_CXX_STANDARD=17 \
     -DCMAKE_INSTALL_LIBDIR=lib \
     -DCMAKE_PREFIX_PATH=${PREFIX} \
     -DBUILD_SHARED_LIBS=ON \
     -DABSL_PROPAGATE_CXX_STD=ON \
-    -GNinja \
     ..
 
 cmake --build .
